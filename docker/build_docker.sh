4c4170a86a75a6f0b548e6ae5c0135f3d3b8d73e
RamiKrispin <ramkrisp@umich.edu>
2022-09-07 05:48:43 -0700

    update the docker files

---
+++ docker/build_docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Build the docker"
+
+docker build . --progress=plain \
+               --build-arg QUARTO_VERSION=1.1.149 \
+               --build-arg CONDA_ENV=flex_dashboard \
+               --build-arg PYTHON_VER=3.8 \
+               -t rkrispin/us_electricity_etl:dev.0.0.0.9000
+
+if [[ $? = 0 ]] ; then
+echo "Pushing docker..."
+docker push rkrispin/us_electricity_etl:dev.0.0.0.9000
+else
+echo "Docker build failed"
+fi
