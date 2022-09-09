52fae70b92d6fb2cdea28465c61b49c4ee0f9e7e
RamiKrispin <ramkrisp@umich.edu>
2022-09-09 08:54:54 -0700

    update the bash script

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -4,8 +4,8 @@ echo "Build the docker"
 
 docker build . --progress=plain \
                --build-arg QUARTO_VERSION=1.1.149 \
-               --build-arg CONDA_ENV=flex_dashboard \
-               --build-arg PYTHON_VER=3.8 \
+               --build-arg CONDA_ENV=us_electricity_etl \
+               --build-arg PYTHON_VER=3.10 \
                -t rkrispin/us_electricity_etl:dev.0.0.0.9000
 
 if [[ $? = 0 ]] ; then
